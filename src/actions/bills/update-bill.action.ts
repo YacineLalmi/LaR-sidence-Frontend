a67f02581d37bd0d715feff17066e92973f04ba7
Yacine LALMI <YacineLALMI19@gmail.com>
2026-04-25 23:09:38 +0100

    fix update bill

--- src/actions/bills/update-bill.action.ts
+++ src/actions/bills/update-bill.action.ts
@@ -7,7 +7,22 @@ import { BillService } from "@/services/bill.service";
 
 export async function updateBillAction(data: BillForm, id: string): Promise<FormState> {
   try {
-    await BillService.update(data, id);
+
+    const formData = new FormData()
+    formData.append("due_date", data.due_date)
+    formData.append("client_id", data.client_id)
+    formData.append("bien_id", data.bien_id)
+    formData.append("status_id", data.status_id)
+    formData.append("services_description", data.services_description || "")
+    formData.append("amount_ht", data.amount_ht)
+    formData.append("amount_tva", data.amount_tva)
+    formData.append("amount_ttc", data.amount_ttc)
+    formData.append("billing_model_id", data.billing_model_id)
+    data.documents.forEach((file: File, index: number) => {
+      formData.append(`documents[${index}]`, file);
+    });
+
+    await BillService.update(formData, id);
     return { isOk: true };
   } catch (error) {
     const result = await handleServerActionError(error);
